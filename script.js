1916f87836175132f1dd6a6594b5b7398eacce90
princemalikxp <103148219+princemalikxp@users.noreply.github.com>
2023-11-25 10:27:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, see you on the 18th!";
+  question.innerHTML = "Yay😭,I LOVE YOU MELODY, see you after your exams ✨!";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
