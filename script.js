8c1bc64416a3c73567a69f0ca70f64aed602d32a
Malik <103148219+princemalikxp@users.noreply.github.com>
2023-12-01 00:14:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "YES✨I LOVE YOU TOO, see you after your exams then😭✨";
+  question.innerHTML = "YES?👀✨I LOVE YOU TOO😭🤌🏻, see you after your exams then😭✨";
   gif.src = "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
   
   // Set the original color
