f194f80966aa6a5b6d56f62411d2b7a7ee25f976
Malik <103148219+princemalikxp@users.noreply.github.com>
2023-12-10 20:53:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "YES?👀✨ really? ayoo😭✨, see you after your exams then😭✨";
+  question.innerHTML = "YES?👀✨ really? ayoo😭✨, see you on our date then✨";
   gif.src = "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
   
   // Set the original color
